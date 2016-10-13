7b93bd91816184698a3ddd5132c80b8bbb7831d1
Clemich38 <clement.letreguilly@gmail.com>
2016-10-13 15:48:53 +0900

    new info popup page for hiragana page

---
+++ src/pages/info/info.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+
+@Component({
+  selector: 'page-info',
+  templateUrl: 'info.html'
+})
+export class InfoPage {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello Info Page');
+  }
+
+}
