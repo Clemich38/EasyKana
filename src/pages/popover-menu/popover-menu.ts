c7640fe4d78acbbefbb333c968885cf80674a548
Clemich38 <clement.letreguilly@gmail.com>
2016-11-01 20:24:25 +0900

    save all the kanas

--- src/pages/popover-menu/popover-menu.ts
+++ src/pages/popover-menu/popover-menu.ts
@@ -9,22 +9,24 @@ import { Storage } from '@ionic/storage';
 })
 export class PopoverMenuPage {
 
-  hiraganaTab: Array<{hiragana: string, katakana: string, romanji: string, hiraganaIsLearned: boolean, katakanaIsLearned: boolean}>;
+  kanaTab: Array<{hiragana: string, katakana: string, romanji: string, hiraganaIsLearned: boolean, katakanaIsLearned: boolean}>;
   
   constructor(public viewCtrl: ViewController,
               public storage: Storage,
               public kanas: Kanas)
   {
-    this.hiraganaTab = kanas.getTab();
+    this.kanaTab = kanas.getTab(0);
   }
 
   selectAll()
   {
-    for(let hir of this.hiraganaTab)
+    for(let hir of this.kanaTab)
       hir.hiraganaIsLearned = true;
 
     // Save it
-    this.kanas.saveData();
+    this.kanas.saveData(0);
+    this.kanas.saveData(1);
+    this.kanas.saveData(2);
 
     // Close popover
     this.viewCtrl.dismiss();
@@ -32,11 +34,13 @@ export class PopoverMenuPage {
 
   selectNone()
   {
-    for(let hir of this.hiraganaTab)
+    for(let hir of this.kanaTab)
       hir.hiraganaIsLearned = false;
 
     // Save it
-    this.kanas.saveData();
+    this.kanas.saveData(0);
+    this.kanas.saveData(1);
+    this.kanas.saveData(2);
 
     // Close popover
     this.viewCtrl.dismiss();
