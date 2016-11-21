67fd1d12a9b8e2b1f2dbcce317654c20c01e082f
Clemich38 <clement.letreguilly@gmail.com>
2016-11-21 14:11:10 +0900

    Add group selection

--- src/pages/popover-menu/popover-menu.ts
+++ src/pages/popover-menu/popover-menu.ts
@@ -10,23 +10,45 @@ import { Storage } from '@ionic/storage';
 export class PopoverMenuPage {
 
   kanaTab: Array<{hiragana: string, katakana: string, romanji: string, hiraganaIsLearned: boolean, katakanaIsLearned: boolean}>;
+  kanaTabV: Array<{hiragana: string, katakana: string, romanji: string, hiraganaIsLearned: boolean, katakanaIsLearned: boolean}>;
+  kanaTabC: Array<{hiragana: string, katakana: string, romanji: string, hiraganaIsLearned: boolean, katakanaIsLearned: boolean}>;
   
   constructor(public viewCtrl: ViewController,
               public storage: Storage,
               public kanas: Kanas)
   {
     this.kanaTab = kanas.getTab(0);
+    this.kanaTabV = kanas.getTab(1);
+    this.kanaTabC = kanas.getTab(2);
   }
 
-  selectAll()
+  selectAll(category)
   {
+    if((category >= 0) && (category <= 2))
+    {
+      this.kanaTab = this.kanas.getTab(category);
+    }
+    else if(category === 3)
+    {
+      this.kanaTab = this.kanas.getTab(0);
+      this.kanaTab = this.kanaTab.concat(this.kanas.getTab(1));
+      this.kanaTab = this.kanaTab.concat(this.kanas.getTab(2));
+    }
+    else
+      return;
+
     for(let hir of this.kanaTab)
       hir.hiraganaIsLearned = true;
 
     // Save it
-    this.kanas.saveData(0);
-    this.kanas.saveData(1);
-    this.kanas.saveData(2);
+    if((category >= 0) && (category <= 2))
+    {
+      this.kanas.saveData(category);
+    }
+    else if(category === 3)
+    {
+      this.saveAll();
+    }
 
     // Close popover
     this.viewCtrl.dismiss();
@@ -34,16 +56,27 @@ export class PopoverMenuPage {
 
   selectNone()
   {
+    // Get all kanas
+    this.kanaTab = this.kanas.getTab(0);
+    this.kanaTab = this.kanaTab.concat(this.kanas.getTab(1));
+    this.kanaTab = this.kanaTab.concat(this.kanas.getTab(2));
+
+    // Deselect all
     for(let hir of this.kanaTab)
       hir.hiraganaIsLearned = false;
 
     // Save it
-    this.kanas.saveData(0);
-    this.kanas.saveData(1);
-    this.kanas.saveData(2);
+    this.saveAll();
 
     // Close popover
     this.viewCtrl.dismiss();
+  }
+
+  saveAll()
+  {
+    this.kanas.saveData(0);
+    this.kanas.saveData(1);
+    this.kanas.saveData(2);
   }
 
 }
