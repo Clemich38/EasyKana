14d34867c9aa09bc675ac93b9c8b27bff18e4b53
Clemich38 <clement.letreguilly@gmail.com>
2016-11-22 17:34:30 +0900

    AdMob banner added

--- src/pages/quiz/quiz.ts
+++ src/pages/quiz/quiz.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 import { SettingsPage } from '../settings/settings';
+
+declare var AdMob: any;
 
 @Component({
   selector: 'page-quiz',
@@ -9,11 +11,55 @@ import { SettingsPage } from '../settings/settings';
 })
 export class QuizPage {
 
-  constructor(public navCtrl: NavController) {}
+  private admobId: any;
 
+  constructor(public navCtrl: NavController, public platform: Platform) {
+    this.platform = platform;
+
+    if (/(android)/i.test(navigator.userAgent))
+      this.admobId = { banner: 'ca-app-pub-4689559823917632/6665370302' };
+
+    else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent))
+      this.admobId = { banner: 'ca-app-pub-4689559823917632/9618836708' };
+
+    this.createBanner();
+    this.showBanner("bottom");
+
+  }
+
+  createBanner() {
+    this.platform.ready().then(() => {
+      if (AdMob) {
+        AdMob.createBanner({
+          adId: this.admobId.banner,
+          autoShow: false
+        });
+      }
+    });
+  }
+
+  showBanner(position) {
+    this.platform.ready().then(() => {
+      if (AdMob) {
+        var positionMap = {
+          "bottom": AdMob.AD_POSITION.BOTTOM_CENTER,
+          "top": AdMob.AD_POSITION.TOP_CENTER
+        };
+        AdMob.showBanner(positionMap[position.toLowerCase()]);
+      }
+    });
+  }
+
+  hideBanner(position) {
+    this.platform.ready().then(() => {
+      if (AdMob) {
+        AdMob.hideBanner();
+      }
+    });
+  }
 
   openSettings(type) {
-    this.navCtrl.push(SettingsPage, {type: type});
+    this.navCtrl.push(SettingsPage, { type: type });
   }
 
   ionViewDidLoad() {
